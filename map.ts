e3785a27271f501aa19b5e761087f87ce1594a01
syndercombecreswell <syndercombecreswell@gmail.com>
2022-11-08 10:31:57 +0000

    second trial

--- map.ts
+++ map.ts
@@ -1,8 +1,20 @@
-function ourMap(inputArr:any [], callbackFn: Function):any []{
-    const resultArr: any [] = [];
+function ourMap(inputArr: number [], callbackFn: Function):any []{
+    const resultArr: number [] = [];
     for(let element of inputArr){
         resultArr.push(callbackFn(element));
     }
     
     return resultArr;
 }
+
+function addFive(number: number): number {
+    return number + 5;
+}
+
+function timesFive(number: number): number {
+    return number * 5;
+}
+
+
+console.log(ourMap([1,2,3,4],addFive));
+console.log(ourMap([1,2,3,4],timesFive));
